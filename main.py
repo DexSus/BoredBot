3488963f9c11b8196b6bab6723dc7ccb386a66b1
DexSus <insomniadex@gmail.com>
2024-02-20 15:12:24 +0200

    bug fix: telegram token

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
 from my_bot.bot import AntiBoredBot
 
 if __name__ == "__main__":
-    bot = AntiBoredBot("6708964967:AAE-KnKJcwfEj4ouNuYCS20z6nSa12O6I9Y")
+    bot = AntiBoredBot("your_token")
     bot.run()
